301edd202f8fc37a9431568aa009fced7ce9571a
Lucio Assis <luciolucio@gmail.com>
2020-07-20 10:31:03 -0300

    Refactor check

--- src/com/piposaude/calendars/check.clj
+++ src/com/piposaude/calendars/check.clj
@@ -1,18 +1,22 @@
 (ns com.piposaude.calendars.check
   (:require [clojure.string :as str]))
 
+(defn- blank-lines? [lines]
+  (or (empty? lines) (some str/blank? lines)))
+
 (defn- too-many-pipes? [index line]
   (when (> (get (frequencies line) \|) 1) index))
 
 (defn- get-first-line-with-too-many-pipes [lines]
-  (when (and (seq lines) (every? (comp not str/blank?) lines))
-    (+ 1 (first (keep-indexed too-many-pipes? lines)))))
+  (+ 1 (first (keep-indexed too-many-pipes? lines))))
 
 (defn get-holiday-file-errors [filename]
   (with-open [reader (clojure.java.io/reader filename)]
-    (let [lines (line-seq reader)
-          first-line-with-too-many-pipes (get-first-line-with-too-many-pipes lines)]
-      (cond (empty? lines) "Holiday file cannot be empty"
-            (some str/blank? lines) "Holiday file cannot have blank lines"
+    (let [lines (line-seq reader)]
+      (if (blank-lines? lines)
+        (cond (empty? lines) "Holiday file cannot be empty"
+              (some str/blank? lines) "Holiday file cannot have blank lines")
+        (let [first-line-with-too-many-pipes (get-first-line-with-too-many-pipes lines)]
+          (cond
             first-line-with-too-many-pipes (format "Line %d: holiday definition cannot have two pipes" first-line-with-too-many-pipes)
-            :else nil))))
+            :default nil))))))
