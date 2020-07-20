a5ca3673ab932d0c54c4feaa005e753307aa59c9
Lucio Assis <luciolucio@gmail.com>
2020-07-20 10:25:21 -0300

    Test for too many pipes

--- src/com/piposaude/calendars/check.clj
+++ src/com/piposaude/calendars/check.clj
@@ -1,10 +1,18 @@
 (ns com.piposaude.calendars.check
   (:require [clojure.string :as str]))
 
+(defn- too-many-pipes? [index line]
+  (when (> (get (frequencies line) \|) 1) index))
+
+(defn- get-first-line-with-too-many-pipes [lines]
+  (when (and (seq lines) (every? (comp not str/blank?) lines))
+    (+ 1 (first (keep-indexed too-many-pipes? lines)))))
+
 (defn get-holiday-file-errors [filename]
   (with-open [reader (clojure.java.io/reader filename)]
     (let [lines (line-seq reader)
-          line-count (count lines)]
-      (cond (<= line-count 0) "Holiday file cannot be empty"
+          first-line-with-too-many-pipes (get-first-line-with-too-many-pipes lines)]
+      (cond (empty? lines) "Holiday file cannot be empty"
             (some str/blank? lines) "Holiday file cannot have blank lines"
+            first-line-with-too-many-pipes (format "Line %d: holiday definition cannot have two pipes" first-line-with-too-many-pipes)
             :else nil))))
--- test/com/piposaude/calendars/check_holiday_file_test.clj
+++ test/com/piposaude/calendars/check_holiday_file_test.clj
@@ -5,6 +5,9 @@
 (deftest should-get-holiday-file-errors-correctly
   (are [expected filename]
     (= expected (check/get-holiday-file-errors (str "test-resources/" filename)))
-    "Holiday file cannot be empty" "empty-file.hol"
-    "Holiday file cannot have blank lines" "blank-file.hol"
-    nil "single-line-valid-file.hol"))
+    "Holiday file cannot be empty" "empty.hol"
+    "Holiday file cannot have blank lines" "blank.hol"
+    "Holiday file cannot have blank lines" "blank2.hol"
+    "Holiday file cannot have blank lines" "blank3.hol"
+    "Line 1: holiday definition cannot have two pipes" "two-pipes.hol"
+    "Line 3: holiday definition cannot have two pipes" "two-pipes2.hol"))
