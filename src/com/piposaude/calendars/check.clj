e7b018cfac7d4d330398097f54bf888f50888a69
Lucio Assis <luciolucio@gmail.com>
2020-07-28 16:22:54 -0300

    Implement nonexistent include check

--- src/com/piposaude/calendars/check.clj
+++ src/com/piposaude/calendars/check.clj
@@ -1,7 +1,9 @@
 (ns com.piposaude.calendars.check
   (:require [instaparse.core :as insta]
             [clojure.edn :as edn]
-            [com.piposaude.calendars.constants :refer [PARSER-GRAMMAR-FILENAME]]))
+            [com.piposaude.calendars.constants :refer [PARSER-GRAMMAR-FILENAME]])
+  (:import (java.nio.file Paths)
+           (java.io FileNotFoundException)))
 
 (defn leap-year? [[_ year-str]]
   (let [year (edn/read-string year-str)]
@@ -23,18 +25,32 @@
 (defn contains-bad-leap-dates [result]
   (some true? (map contains-bad-leap-date result)))
 
-(defn contains-bad-includes [result]
-  (some true? (map contains-bad-include result)))
+(defn included-holiday-exists [result including-file]
+  (let [[type holiday-name & _] (first result)
+        including-file-path (.toString (.getParent (Paths/get including-file (into-array String []))))
+        filename (.toString (Paths/get including-file-path (into-array String [(str holiday-name ".hol")])))]
+    (if (= :include type)
+      (try
+        (boolean (clojure.java.io/reader filename))
+        (catch FileNotFoundException e
+          false))
+      true)))
 
 (defn get-errors [filename]
   (let [parser (insta/parser (clojure.java.io/resource PARSER-GRAMMAR-FILENAME))
         result (parser (slurp filename))]
     (insta/get-failure result)))
 
+(defn drop-include [result]
+  (let [[type & _] (first result)]
+    (if (= :include type)
+      (rest result)
+      result)))
+
 (defn valid-holiday-file? [filename]
   (let [parser (insta/parser (clojure.java.io/resource PARSER-GRAMMAR-FILENAME))
         result (parser (slurp filename))]
     (and
       (not (insta/failure? result))
-      (not (contains-bad-leap-dates result))
-      (not (contains-bad-includes result)))))
+      (not (contains-bad-leap-dates (drop-include result)))
+      (included-holiday-exists result filename))))
