62d566e27a599a391064fe7a741e60a41474ae28
Lucio Assis <luciolucio@gmail.com>
2020-07-29 09:59:32 -0300

    Add tests for SPO 2020

--- README.md
+++ README.md
@@ -3,7 +3,5 @@
 Creates calendars in the event store, to be used with `rdate`
 
 ## Next steps
-* Add SPO tests, including the craziness that is 2020
-  - See http://www.feriados.com.br/feriados-sao_paulo-sp.php
 * Build logic so files don't get overwritten
 * Build CI scripts
--- test/com/piposaude/calendars/generate_test.clj
+++ test/com/piposaude/calendars/generate_test.clj
@@ -50,6 +50,26 @@
     [{:name "Easter" :date (t/date "2017-04-16")}] 2017
     [{:name "Easter" :date (t/date "2018-04-01")}] 2018))
 
+(deftest should-generate-holidays-when-holidays-for-year-with-nested-includes
+  (= [{:name "Holiday from include-first" :date (t/date "2012-01-25")}
+      {:name "Holiday from include-second" :date (t/date "2012-01-30")}
+      {:name "Holiday from include-third" :date (t/date "2012-02-01")}]
+     (gen/holidays-for-year 2012 "test-resources/generate/include-first.hol")))
+
+(deftest should-generate-holidays-when-holidays-for-year-with-exception-on-expression
+  (are [expected year]
+    (= expected (gen/holidays-for-year year (str "test-resources/generate/exception-on-expression.hol")))
+    [{:name "Easter" :date (t/date "2012-04-08")}] 2012
+    [] 2013
+    [{:name "Easter" :date (t/date "2014-04-20")}] 2014))
+
+(deftest should-generate-holidays-when-holidays-for-year-with-exception-on-ddmmm
+  (are [expected year]
+    (= expected (gen/holidays-for-year year (str "test-resources/generate/exception-on-ddmmm.hol")))
+    [{:name "NATIONAL PEANUT BUTTER AND JELLY DAY" :date (t/date "2012-04-02")}] 2012
+    [] 2013
+    [{:name "NATIONAL PEANUT BUTTER AND JELLY DAY" :date (t/date "2014-04-02")}] 2014))
+
 (deftest should-generate-holidays-when-holidays-for-year-with-brazil-holidays
   (are [year expected]
     (= expected (gen/holidays-for-year year "test-resources/generate/BR.hol"))
@@ -95,24 +115,20 @@
           {:name "Natal" :date (t/date "2012-12-25")}
           {:name "Aniversário de São Paulo" :date (t/date "2012-01-25")}
           {:name "Revolução Constitucionalista" :date (t/date "2012-07-09")}
-          {:name "Dia da Consciência Negra" :date (t/date "2012-11-20")}]))
-
-(deftest should-generate-holidays-when-holidays-for-year-with-nested-includes
-  (= [{:name "Holiday from include-first" :date (t/date "2012-01-25")}
-      {:name "Holiday from include-second" :date (t/date "2012-01-30")}
-      {:name "Holiday from include-third" :date (t/date "2012-02-01")}]
-     (gen/holidays-for-year 2012 "test-resources/generate/include-first.hol")))
-
-(deftest should-generate-holidays-when-holidays-for-year-with-exception-on-expression
-  (are [expected year]
-    (= expected (gen/holidays-for-year year (str "test-resources/generate/exception-on-expression.hol")))
-    [{:name "Easter" :date (t/date "2012-04-08")}] 2012
-    [] 2013
-    [{:name "Easter" :date (t/date "2014-04-20")}] 2014))
-
-(deftest should-generate-holidays-when-holidays-for-year-with-exception-on-ddmmm
-  (are [expected year]
-    (= expected (gen/holidays-for-year year (str "test-resources/generate/exception-on-ddmmm.hol")))
-    [{:name "NATIONAL PEANUT BUTTER AND JELLY DAY" :date (t/date "2012-04-02")}] 2012
-    [] 2013
-    [{:name "NATIONAL PEANUT BUTTER AND JELLY DAY" :date (t/date "2014-04-02")}] 2014))
+          {:name "Dia da Consciência Negra" :date (t/date "2012-11-20")}]
+    2020 [{:name "Confraternização Universal / Ano Novo" :date (t/date "2020-01-01")}
+          {:name "Segunda-feira de carnaval" :date (t/date "2020-02-24")}
+          {:name "Terça-feira de carnaval" :date (t/date "2020-02-25")}
+          {:name "Sexta-feira da Paixão" :date (t/date "2020-04-10")}
+          {:name "Tiradentes" :date (t/date "2020-04-21")}
+          {:name "Dia do Trabalho" :date (t/date "2020-05-01")}
+          {:name "Independência do Brasil" :date (t/date "2020-09-07")}
+          {:name "Nossa Senhora de Aparecida" :date (t/date "2020-10-12")}
+          {:name "Finados" :date (t/date "2020-11-02")}
+          {:name "Proclamação da República" :date (t/date "2020-11-15")}
+          {:name "Natal" :date (t/date "2020-12-25")}
+          {:name "Aniversário de São Paulo" :date (t/date "2020-01-25")}
+          {:name "Antecipação Corpus Christi" :date (t/date "2020-05-20")}
+          {:name "Antecipação Consciência Negra" :date (t/date "2020-05-21")}
+          {:name "Emenda Quarentena" :date (t/date "2020-05-22")}
+          {:name "Antecipação Revolução Constitucionalista" :date (t/date "2020-05-25")}]))
