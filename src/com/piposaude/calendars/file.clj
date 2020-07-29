97f2a5face73745cf9348b75ea25e8b50e040e9c
Lucio Assis <luciolucio@gmail.com>
2020-07-28 22:37:45 -0300

    Generate file and save into store

--- src/com/piposaude/calendars/file.clj
+++ src/com/piposaude/calendars/file.clj
@@ -1,6 +1,27 @@
 (ns com.piposaude.calendars.file
-  (:require [clojure.java.io :as io]
-            [com.piposaude.components.store.api :as store.api]))
+  (:require [com.piposaude.components.store.api :as store.api]
+            [com.piposaude.calendars.generate :as gen]
+            [clojure.string :as str]
+            [tick.alpha.api :as t])
+  (:import (java.nio.file Paths)))
+
+(defn gen-store-path [holiday-file output-path]
+  (let [filename (.toString (.getFileName (Paths/get holiday-file (into-array String []))))
+        filename-sans-extension (str/join "" (drop-last 4 filename))]
+    (.toString (Paths/get output-path (into-array String [filename-sans-extension])))))
+
+(defn format-holiday [holiday]
+  (t/format (tick.format/formatter "yyyyMMdd") (:date holiday)))
+
+(defn get-holidays [holiday-file year]
+  (map format-holiday (gen/holidays-for-year year holiday-file)))
+
+(defn gen-bracketed-holidays [holiday-file year bracket-size]
+  (let [years (range (- year bracket-size) (+ year (inc bracket-size)))
+        holidays (flatten (map (partial get-holidays holiday-file) years))]
+    (str/join "\n" holidays)))
 
 (defn generate [store holiday-file year bracket-size output-path]
-  (store.api/store-object! store "file/output/FILE" "20190728\n20200728\n20210728"))
+  (let [holidays (gen-bracketed-holidays holiday-file year bracket-size)
+        store-path (gen-store-path holiday-file output-path)]
+    (store.api/store-object! store store-path holidays)))
--- test/com/piposaude/calendars/file_test.clj
+++ test/com/piposaude/calendars/file_test.clj
@@ -2,10 +2,15 @@
   (:require [clojure.test :refer :all]
             [com.piposaude.calendars.file :as file]
             [com.stuartsierra.component :as component]
-            [com.piposaude.components.store.impl.in-memory-store :as inmm]
+            [com.piposaude.components.store.impl.in-memory-store :as store]
             [com.piposaude.components.store.api :as store.api]))
 
-(deftest should-generate-calendar-file-correctly
-  (let [store (component/start (inmm/map->InMemoryStore {:settings {:base-dir "test-resources"}}))]
-    (file/generate store "test-resources/file/FILE.hol" 2020 1 "file/output")
-    (is (= "20190728\n20200728\n20210728" (slurp (:input-stream (store.api/fetch-object store "file/output/FILE")))))))
+(def store (component/start (store/map->InMemoryStore {:settings {}})))
+
+(deftest should-generate-calendar-file-when-generate
+  (file/generate store "test-resources/file/FILE.hol" 2020 1 "file/output")
+  (is (= "20190728\n20200728\n20210728" (slurp (:input-stream (store.api/fetch-object store "file/output/FILE"))))))
+
+(deftest should-generate-calendar-file-with-larger-bracket-when-generate
+  (file/generate store "test-resources/file/FILE.hol" 2020 2 "file/output")
+  (is (= "20180728\n20190728\n20200728\n20210728\n20220728" (slurp (:input-stream (store.api/fetch-object store "file/output/FILE"))))))
