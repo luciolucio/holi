88ec1b6f37c7aa534508e5ea8e95d3bd77bb8432
Lucio Assis <luciolucio@gmail.com>
2020-07-28 22:04:23 -0300

    Use store

--- src/com/piposaude/calendars/file.clj
+++ src/com/piposaude/calendars/file.clj
@@ -1,6 +1,6 @@
 (ns com.piposaude.calendars.file
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [com.piposaude.components.store.api :as store.api]))
 
-(defn generate [holiday-file year bracket-size output-dir]
-  (io/make-parents "test-resources/file/output/FILE")
-  (spit "test-resources/file/output/FILE" "20190728\n20200728\n20210728"))
+(defn generate [store holiday-file year bracket-size output-path]
+  (store.api/store-object! store "file/output/FILE" "20190728\n20200728\n20210728"))
--- test/com/piposaude/calendars/file_test.clj
+++ test/com/piposaude/calendars/file_test.clj
@@ -1,7 +1,11 @@
 (ns com.piposaude.calendars.file-test
   (:require [clojure.test :refer :all]
-            [com.piposaude.calendars.file :as file]))
+            [com.piposaude.calendars.file :as file]
+            [com.stuartsierra.component :as component]
+            [com.piposaude.components.store.impl.in-memory-store :as inmm]
+            [com.piposaude.components.store.api :as store.api]))
 
 (deftest should-generate-calendar-file-correctly
-  (file/generate "test-resources/file/FILE.hol" 2020 1 "test-resources/file/output")
-  (= "20190728\n20200728\n20210728" (slurp "test-resources/file/output/FILE")))
+  (let [store (component/start (inmm/map->InMemoryStore {:settings {:base-dir "test-resources"}}))]
+    (file/generate store "test-resources/file/FILE.hol" 2020 1 "file/output")
+    (is (= "20190728\n20200728\n20210728" (slurp (:input-stream (store.api/fetch-object store "file/output/FILE")))))))
