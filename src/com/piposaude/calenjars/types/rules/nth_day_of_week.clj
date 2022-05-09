3c4b409c5f07a6e880501e059c33c429225a8c57
Lucio Assis <luciolucio@gmail.com>
2022-05-08 23:54:28 -0300

    Fix formatting

--- src/com/piposaude/calenjars/types/rules/nth_day_of_week.clj
+++ src/com/piposaude/calenjars/types/rules/nth_day_of_week.clj
@@ -18,12 +18,12 @@
         i-as-int (edn/read-string i)
         intvl (t/bounds (t/year-month (LocalDate/of year month-as-int 1)))
         week-beginnings (t/range
-                          (t/date (t/beginning intvl))
-                          (t/date (t/end intvl))
-                          (t/new-period 1 :weeks))
+                         (t/date (t/beginning intvl))
+                         (t/date (t/end intvl))
+                         (t/new-period 1 :weeks))
         days-in-week (t/range
-                       (nth week-beginnings (dec i-as-int))
-                       (nth week-beginnings i-as-int)
-                       (t/new-period 1 :days))
+                      (nth week-beginnings (dec i-as-int))
+                      (nth week-beginnings i-as-int)
+                      (t/new-period 1 :days))
         day (first (filter #(= (t/day-of-week %) (day-of-week-str->day-of-week day-of-week-str)) days-in-week))]
     (common/holiday holiday-name (str (t/day-of-month day)) (str month-as-int) year)))
