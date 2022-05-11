b2348f4ac252d7e4803311a23728ce63226fcd07
Lucio Assis <luciolucio@gmail.com>
2022-05-11 20:11:18 -0300

    Add support for nth-to-last day of week

--- src/com/piposaude/calenjars/types/rules/nth_day_of_week.clj
+++ src/com/piposaude/calenjars/types/rules/nth_day_of_week.clj
@@ -14,19 +14,16 @@
    "Sat" t/SATURDAY})
 
 (defn get-holiday-nth-day-of-week [^Integer year holiday-name [i day-of-week-str month]]
-  (let [^Integer month-as-int (-> month common/month->month-number edn/read-string)
-        i-as-int (edn/read-string i)
-        month-bounds (t/bounds (t/year-month (LocalDate/of year month-as-int 1)))
-        week-beginnings (t/range
-                         (t/date (t/beginning month-bounds))
-                         (t/date (t/end month-bounds))
-                         (t/new-period 1 :weeks))
-        days-in-week (t/range
-                      (nth week-beginnings (dec i-as-int))
-                      (nth week-beginnings i-as-int)
-                      (t/new-period 1 :days))
+  (let [i-as-int (edn/read-string i)
         day-of-week (day-of-week-str->day-of-week day-of-week-str)
-        holiday (->> days-in-week
-                     (filter #(= (t/day-of-week %) day-of-week))
-                     first)]
-    (common/holiday holiday-name (str (t/day-of-month holiday)) (str month-as-int) year)))
+        ^Integer month-as-int (-> month common/month->month-number edn/read-string)
+        month-bounds (t/bounds (t/year-month (LocalDate/of year month-as-int 1)))
+        month-days (t/range
+                    (t/date (t/beginning month-bounds))
+                    (t/date (t/end month-bounds))
+                    (t/new-period 1 :days))
+        month-days-that-are-day-of-week (filterv #(= (t/day-of-week %) day-of-week) month-days)
+        index (if (pos? i-as-int) (dec i-as-int) (+ (count month-days-that-are-day-of-week) i-as-int))
+        holiday (when (contains? month-days-that-are-day-of-week index) (nth month-days-that-are-day-of-week index))]
+    (when holiday
+      (common/holiday holiday-name (str (t/day-of-month holiday)) (str month-as-int) year))))
--- test/com/piposaude/calenjars/holidays_test.clj
+++ test/com/piposaude/calenjars/holidays_test.clj
@@ -53,16 +53,42 @@
 (deftest should-generate-holidays-when-holidays-for-year-with-nth-day-of-week-rule
   (are [expected year]
        (= expected (gen/holidays-for-year year "test-resources/generate/rule-nth-day-of-week.hol"))
-    [{:name "Thanksgiving" :date (t/date "2016-11-24")}
+    [{:name "Memorial Day" :date (t/date "2016-05-30")}
+     {:name "Thanksgiving" :date (t/date "2016-11-24")}
      {:name "Columbus Day" :date (t/date "2016-10-10")}] 2016
-    [{:name "Thanksgiving" :date (t/date "2017-11-23")}
+    [{:name "Memorial Day" :date (t/date "2017-05-29")}
+     {:name "Thanksgiving" :date (t/date "2017-11-23")}
      {:name "Columbus Day" :date (t/date "2017-10-09")}] 2017
-    [{:name "Thanksgiving" :date (t/date "2018-11-22")}
+    [{:name "Memorial Day" :date (t/date "2018-05-28")}
+     {:name "Thanksgiving" :date (t/date "2018-11-22")}
      {:name "Columbus Day" :date (t/date "2018-10-08")}] 2018
-    [{:name "Thanksgiving" :date (t/date "2019-11-28")}
+    [{:name "Memorial Day" :date (t/date "2019-05-27")}
+     {:name "Thanksgiving" :date (t/date "2019-11-28")}
      {:name "Columbus Day" :date (t/date "2019-10-14")}] 2019
-    [{:name "Thanksgiving" :date (t/date "2020-11-26")}
-     {:name "Columbus Day" :date (t/date "2020-10-12")}] 2020))
+    [{:name "Memorial Day" :date (t/date "2020-05-25")}
+     {:name "Thanksgiving" :date (t/date "2020-11-26")}
+     {:name "Columbus Day" :date (t/date "2020-10-12")}] 2020)
+
+  (are [expected year]
+       (= expected (gen/holidays-for-year year "test-resources/generate/rule-nth-day-of-week-limit-cases.hol"))
+    [{:name "Inexistent Holiday 1" :date (t/date "2021-05-03")}
+     {:name "Inexistent Holiday 2" :date (t/date "2021-05-10")}
+     {:name "Inexistent Holiday 3" :date (t/date "2021-05-17")}
+     {:name "Inexistent Holiday 4" :date (t/date "2021-05-24")}
+     {:name "Inexistent Holiday 5" :date (t/date "2021-05-31")}
+     {:name "Inexistent Holiday -1" :date (t/date "2021-05-31")}
+     {:name "Inexistent Holiday -2" :date (t/date "2021-05-24")}
+     {:name "Inexistent Holiday -3" :date (t/date "2021-05-17")}
+     {:name "Inexistent Holiday -4" :date (t/date "2021-05-10")}
+     {:name "Inexistent Holiday -5" :date (t/date "2021-05-03")}] 2021
+    [{:name "Inexistent Holiday 1" :date (t/date "2019-05-06")}
+     {:name "Inexistent Holiday 2" :date (t/date "2019-05-13")}
+     {:name "Inexistent Holiday 3" :date (t/date "2019-05-20")}
+     {:name "Inexistent Holiday 4" :date (t/date "2019-05-27")}
+     {:name "Inexistent Holiday -1" :date (t/date "2019-05-27")}
+     {:name "Inexistent Holiday -2" :date (t/date "2019-05-20")}
+     {:name "Inexistent Holiday -3" :date (t/date "2019-05-13")}
+     {:name "Inexistent Holiday -4" :date (t/date "2019-05-06")}] 2019))
 
 (deftest should-generate-holidays-when-holidays-for-year-with-nested-includes
   (= [{:name "Holiday from include-first" :date (t/date "2012-01-25")}
