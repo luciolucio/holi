bbb81328d06350b39fb68c50c41e5aa64cb6fa6b
Lucio Assis <luciolucio@gmail.com>
2022-05-22 18:13:49 +0100

    Add test files and first test for expression observance rule

--- test/com/piposaude/calenjars/holidays_expression_easter_test.clj
+++ test/com/piposaude/calenjars/holidays_expression_easter_test.clj
@@ -16,10 +16,21 @@
 
 (deftest should-generate-holidays-when-holidays-for-year-with-exception-on-expression
   (are [expected year]
-       (= expected (gen/holidays-for-year year "test-resources/generate/expression-exception.hol"))
+       (= expected (gen/holidays-for-year year "test-resources/generate/expression-easter-exception.hol"))
     [{:name "Easter" :date (t/date "2012-04-08")}] 2012
     [] 2013
     [{:name "Easter" :date (t/date "2014-04-20")}] 2014))
+
+(deftest should-generate-holidays-when-holidays-for-year-with-easter-expression-and-observance-rule
+  (are [expected year]
+    (= expected (gen/holidays-for-year year "test-resources/generate/expression-easter-observed.hol"))
+    [{:name "Easter" :date (t/date "2012-04-09")}] 2012
+    [{:name "Easter" :date (t/date "2013-04-01")}] 2013
+    [{:name "Easter" :date (t/date "2014-04-21")}] 2014
+    [{:name "Easter" :date (t/date "2015-04-06")}] 2015
+    [{:name "Easter" :date (t/date "2016-03-28")}] 2016
+    [{:name "Easter" :date (t/date "2017-04-17")}] 2017
+    [{:name "Easter" :date (t/date "2018-04-02")}] 2018))
 
 (deftest should-generate-holidays-when-holidays-for-year-with-easter-expression-and-start-clause
   (are [expected year]
