ee328393cd713a3144225095378f786df0e3bebb
Lucio Assis <luciolucio@gmail.com>
2022-05-22 16:37:23 +0100

    Support end year for expression

--- src/com/piposaude/calenjars/types/expression.clj
+++ src/com/piposaude/calenjars/types/expression.clj
@@ -1,9 +1,24 @@
 (ns com.piposaude.calenjars.types.expression
   (:require [com.piposaude.calenjars.types.expressions.easter :as easter]))
 
-(defn get-holiday-by-expression [year name args]
-  (condp = (first args)
+(defn make-holiday [year name args]
+  (case (first args)
     :easter
     (easter/get-holiday-easter year name (rest args))
 
     nil))
+
+(defn get-holiday-by-expression [year name args start-year end-year]
+  (let [holiday (make-holiday year name args)]
+    (cond
+      (and (not start-year) (not end-year))
+      holiday
+
+      (and start-year (>= year start-year))
+      holiday
+
+      (and end-year (<= year end-year))
+      holiday
+
+      :else
+      nil)))
--- test/com/piposaude/calenjars/holidays_expression_test.clj
+++ test/com/piposaude/calenjars/holidays_expression_test.clj
@@ -20,3 +20,52 @@
     [{:name "Easter" :date (t/date "2012-04-08")}] 2012
     [] 2013
     [{:name "Easter" :date (t/date "2014-04-20")}] 2014))
+
+(deftest should-generate-holidays-when-holidays-for-year-with-easter-expression-and-start-clausex
+  (are [expected year]
+       (= expected (gen/holidays-for-year year "test-resources/generate/expression-easter-start.hol"))
+    [] 2012
+    [] 2013
+    [{:name "Easter" :date (t/date "2014-04-20")}] 2014
+    [{:name "Easter" :date (t/date "2015-04-05")}] 2015
+    [{:name "Easter" :date (t/date "2016-03-27")}] 2016
+    [{:name "Easter" :date (t/date "2017-04-16")}] 2017
+    [{:name "Easter" :date (t/date "2018-04-01")}] 2018))
+
+(deftest should-generate-holidays-when-holidays-for-year-with-easter-expression-and-start-clause
+  (are [expected year]
+       (= expected (gen/holidays-for-year year "test-resources/generate/expression-easter-start.hol"))
+    [{:name "Easter" :date (t/date "2014-04-20")}] 2014))
+
+(deftest should-generate-holidays-when-holidays-for-year-with-easter-expression-and-start-clause-and-exception
+  (are [expected year]
+       (= expected (gen/holidays-for-year year "test-resources/generate/expression-easter-start-exception.hol"))
+    [] 2012
+    [] 2013
+    [{:name "Easter" :date (t/date "2014-04-20")}] 2014
+    [{:name "Easter" :date (t/date "2015-04-05")}] 2015
+    [{:name "Easter" :date (t/date "2016-03-27")}] 2016
+    [] 2017
+    [{:name "Easter" :date (t/date "2018-04-01")}] 2018))
+
+(deftest should-generate-holidays-when-holidays-for-year-with-easter-expression-and-end-clause
+  (are [expected year]
+       (= expected (gen/holidays-for-year year "test-resources/generate/expression-easter-end.hol"))
+    [{:name "Easter" :date (t/date "2012-04-08")}] 2012
+    [{:name "Easter" :date (t/date "2013-03-31")}] 2013
+    [{:name "Easter" :date (t/date "2014-04-20")}] 2014
+    [{:name "Easter" :date (t/date "2015-04-05")}] 2015
+    [{:name "Easter" :date (t/date "2016-03-27")}] 2016
+    [] 2017
+    [] 2018))
+
+(deftest should-generate-holidays-when-holidays-for-year-with-easter-expression-and-end-clause-and-exception
+  (are [expected year]
+       (= expected (gen/holidays-for-year year "test-resources/generate/expression-easter-end-exception.hol"))
+    [{:name "Easter" :date (t/date "2012-04-08")}] 2012
+    [] 2013
+    [{:name "Easter" :date (t/date "2014-04-20")}] 2014
+    [{:name "Easter" :date (t/date "2015-04-05")}] 2015
+    [{:name "Easter" :date (t/date "2016-03-27")}] 2016
+    [] 2017
+    [] 2018))
