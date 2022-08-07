e5b6f02b29254315dea51bc56d61641a0f004def
Lucio Assis <luciolucio@gmail.com>
2022-08-07 17:11:38 -0300

    Add BR as a base calendar + generate during release

--- bin/release.sh
+++ bin/release.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
 LIB_NAME="holi"
+CALENDAR_SOURCE_DIR="resources/calendars-source"
+OUTPUT_DIR="resources/calendars-generated"
+
+echo "Starting holiday generation"
+rm -rf $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR
+clojure -M:generate 80 $CALENDAR_SOURCE_DIR $OUTPUT_DIR
 
 echo Creating uberjar: "${LIB_NAME}.jar"
-clojure -A:depstar -m hf.depstar.jar "${LIB_NAME}.jar"
+clojure -M:depstar -m hf.depstar.jar "${LIB_NAME}.jar"
 
 echo Updating pom with latest dependecies
 clojure -Spom
