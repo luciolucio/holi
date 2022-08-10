55e20321f113480b9d0e1dbb0985e1519f44a069
Lucio Assis <luciolucio@gmail.com>
2022-08-09 21:17:21 -0300

    Refactor generation to simplify it

--- src/luciolucio/holi/generate.clj
+++ src/luciolucio/holi/generate.clj
@@ -1,17 +1,19 @@
 (ns luciolucio.holi.generate
   (:require [luciolucio.holi.file :as file]
             [tick.alpha.api :as t]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn])
+  (:import (java.time LocalDate)))
 
-(defn -main [bracket-size calendar-file-dir output-path]
-  (let [today (t/today)
-        current-year (edn/read-string (t/format (tick.format/formatter "yyyy") today))
+(defn -main [bracket calendar-file-dir output-path]
+  (let [^LocalDate today (t/today)
+        current-year (.getYear today)
         directory (clojure.java.io/file calendar-file-dir)
-        [_ & files] (file-seq directory)]
+        [_ & files] (file-seq directory)
+        bracket-size (edn/read-string bracket)]
     (println "-----------------------------------------------------------")
     (println (format "Generating holidays for holiday files under %s" calendar-file-dir))
     (if (empty? files) (println "No holiday files to process"))
-    (run! #(file/generate-datelist! (str %) output-path current-year (edn/read-string bracket-size)) files)
-    (file/generate-weekend-datelist! output-path current-year (edn/read-string bracket-size))
+    (run! #(file/generate-datelist! (str %) output-path current-year bracket-size) files)
+    (file/generate-weekend-datelist! output-path current-year bracket-size)
     (println "Holiday generation finished")
     (println "-----------------------------------------------------------")))
