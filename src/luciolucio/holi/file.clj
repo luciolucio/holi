dca38e93cc1ef3d43f05da5e80cd3926100fe6b7
Lucio Assis <luciolucio@gmail.com>
2022-08-11 21:28:18 -0300

    Support generating files in subdirs

--- src/luciolucio/holi/file.clj
+++ src/luciolucio/holi/file.clj
@@ -3,21 +3,27 @@
             [luciolucio.holi.holidays :as gen]
             [luciolucio.holi.constants :as constants]
             [clojure.string :as str]
-            [tick.alpha.api :as t])
+            [tick.alpha.api :as t]
+            [clojure.java.io :as io])
   (:import (java.nio.file Paths)))
 
 (defn format-YYYYMMDD [holiday]
   (t/format (tick.format/formatter "yyyy-MM-dd") holiday))
 
-(defn gen-datelist-path [filename output-path]
-  (-> (Paths/get output-path (into-array String [filename]))
-      .toString
-      (str constants/DATELIST-EXTENSION)))
+(defn make-datelist-path! [filename output-path]
+  (let [final-path (Paths/get output-path (into-array String [filename]))]
+    (-> (.getParent final-path)
+        .toString
+        io/file
+        .mkdirs)
+    (-> final-path
+        .toString
+        (str constants/DATELIST-EXTENSION))))
 
-(defn gen-datelist-file-path [holiday-file output-path]
-  (let [filename (.toString (.getFileName (Paths/get holiday-file (into-array String []))))
-        output-filename (str/join "" (drop-last 4 filename))]
-    (gen-datelist-path output-filename output-path)))
+(defn make-datelist-file-path! [root-path holiday-file output-path]
+  (let [filename (Paths/get (subs holiday-file (count root-path)) (into-array String []))
+        output-filename (str/join "" (drop-last 4 (.toString filename)))]
+    (make-datelist-path! output-filename output-path)))
 
 (defn get-holidays [holiday-file year]
   (map #(format-YYYYMMDD (:date %)) (gen/holidays-for-year year holiday-file)))
@@ -37,12 +43,14 @@
   (let [years (range (- year bracket-size) (+ year (inc bracket-size)))]
     (flatten (map get-weekends years))))
 
-(defn generate-datelist! [holiday-file output-path year bracket-size]
-  (let [holidays (gen-bracketed-holidays holiday-file year bracket-size)
-        path (gen-datelist-file-path holiday-file output-path)]
-    (spit path (str/join "\n" holidays))))
+(defn generate-datelist! [root-path holiday-file output-path year bracket-size]
+  (if (str/ends-with? (str/lower-case holiday-file) "weekend.hol")
+    (throw (ex-info "WEEKEND.hol is not allowed" {:error :weekend-dot-hol-present}))
+    (let [holidays (gen-bracketed-holidays holiday-file year bracket-size)
+          path (make-datelist-file-path! root-path holiday-file output-path)]
+      (spit path (str/join "\n" holidays)))))
 
 (defn generate-weekend-datelist! [output-path year bracket-size]
   (let [weekends (gen-bracketed-weekends year bracket-size)
-        weekend-path (gen-datelist-path constants/WEEKEND-FILE-NAME output-path)]
+        weekend-path (make-datelist-path! constants/WEEKEND-FILE-NAME output-path)]
     (spit weekend-path (str/join "\n" weekends))))
--- src/luciolucio/holi/generate.clj
+++ src/luciolucio/holi/generate.clj
@@ -2,7 +2,8 @@
   (:require [luciolucio.holi.file :as file]
             [tick.alpha.api :as t]
             [clojure.edn :as edn])
-  (:import (java.time LocalDate)))
+  (:import (java.time LocalDate)
+           (clojure.lang ExceptionInfo)))
 
 (defn -main [bracket calendar-file-dir output-path]
   (let [^LocalDate today (t/today)
@@ -12,8 +13,15 @@
         bracket-size (edn/read-string bracket)]
     (println "-----------------------------------------------------------")
     (println (format "Generating holidays for holiday files under %s" calendar-file-dir))
-    (if (empty? files) (println "No holiday files to process"))
-    (run! #(file/generate-datelist! (str %) output-path current-year bracket-size) files)
+    (when (empty? files) (println "No holiday files to process"))
+    (try
+      (run! #(file/generate-datelist! calendar-file-dir (str %) output-path current-year bracket-size) files)
+      (catch ExceptionInfo e
+        (if (= (:error (ex-data e)) :weekend-dot-hol-present)
+          (do
+            (println "ERROR: WEEKEND.hol is not an allowed file name, please use another name")
+            (System/exit 1))
+          (throw e))))
     (file/generate-weekend-datelist! output-path current-year bracket-size)
     (println "Holiday generation finished")
     (println "-----------------------------------------------------------")))
