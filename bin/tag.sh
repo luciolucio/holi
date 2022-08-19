0915586914febdfad7f24043000ad332624fdf28
Lucio Assis <luciolucio@gmail.com>
2022-08-18 23:53:05 -0300

    Add tagging command

---
+++ bin/tag.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+VERSION_NUMBER=`grep '^[*]' CHANGELOG.md | head -1 | awk -F' ' '{print $2}'`
+
+CHANGELOG=`awk -v ver="$VERSION_NUMBER" '/^[*] / { if (p) { exit }; if ($2 == ver) { p=1 } } p && NF' CHANGELOG.md`
+echo $CHANGELOG > tag.txt
+
+git tag "$VERSION_NUMBER" -F tag.txt
+git push origin "$VERSION_NUMBER"
+
+rm tag.txt
