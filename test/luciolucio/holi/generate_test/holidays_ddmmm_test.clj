25c318c2fb3553bc11debcd9b48e955df7a6b267
Lucio Assis <luciolucio@gmail.com>
2022-09-12 20:52:57 -0300

    Support observed-monday and fix GB with it

--- test/luciolucio/holi/generate_test/holidays_ddmmm_test.clj
+++ test/luciolucio/holi/generate_test/holidays_ddmmm_test.clj
@@ -39,6 +39,17 @@
     [{:name "Independence Day" :date (t/date "2020-07-03")}] 2020
     [{:name "Independence Day" :date (t/date "2021-07-05")}] 2021))
 
+(deftest should-generate-holidays-when-holidays-for-year-with-monday-observance-rule-on-ddmmm
+  (are [expected year]
+       (= expected (gen/holidays-for-year year constants/TEST-ROOT  "test-resources/generate/ddmmm-observed-monday.hol"))
+    [{:name "New Year's Day" :date (t/date "2016-01-01")}] 2016
+    [{:name "New Year's Day" :date (t/date "2017-01-02")}] 2017
+    [{:name "New Year's Day" :date (t/date "2018-01-01")}] 2018
+    [{:name "New Year's Day" :date (t/date "2019-01-01")}] 2019
+    [{:name "New Year's Day" :date (t/date "2020-01-01")}] 2020
+    [{:name "New Year's Day" :date (t/date "2021-01-01")}] 2021
+    [{:name "New Year's Day" :date (t/date "2022-01-03")}] 2022))
+
 (deftest should-generate-holidays-when-holidays-for-year-with-monday-tuesday-observance-rule-on-ddmmm
   (are [expected year]
        (= expected (gen/holidays-for-year year constants/TEST-ROOT  "test-resources/generate/ddmmm-observed-monday-tuesday.hol"))
