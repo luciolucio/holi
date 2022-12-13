d86749a2b491a6745605b3b23227055ef355ee47
Lucio Assis <luciolucio@gmail.com>
2022-12-13 18:00:36 -0300

    Add every calendar in lib tests + update roadmap

--- test-lib/cljs/src/holi_test.cljs
+++ test-lib/cljs/src/holi_test.cljs
@@ -4,7 +4,14 @@
             [cljs.test :as ct]))
 
 (ct/deftest check-calendars
-  (ct/is (true? (holi/holiday? (t/date "2022-10-12") "BR"))))
+  (ct/is (true? (holi/weekend? (t/date "2022-12-11"))))
+
+  (ct/are [calendar date]
+    (true? (holi/holiday? (t/date date) calendar))
+    "BR" "2022-10-12"
+    "US" "2022-07-04"
+    "GB" "2022-06-03"
+    "brazil/sao-paulo" "2022-01-25"))
 
 (defn -main []
   (ct/run-tests 'holi-test))
