363864d3d0cceb9a2d3d010a45c877080a54b060
Lucio Assis <luciolucio@gmail.com>
2023-01-10 17:19:21 -0300

    Make 'calendars-generated' THE place for datelists
    Problem here was that when running tests it worked because it was
    reading datelist files from resources/calendars-generated, but when it
    got built the files went to the root dir and then it wouldn't work.
    
    Now the build puts the files in 'calendars-generated' too. Now just have
    to remove all that duplication of 'calendars-generated' everywhere...

--- resources/holi-template/resources/build.clj
+++ resources/holi-template/resources/build.clj
@@ -7,7 +7,8 @@
 (def basis (b/create-basis {:project "deps.edn"}))
 
 (defn clean [_]
-  (b/delete {:path "target"}))
+  (b/delete {:path "target"})
+  (b/delete {:path "calendars-generated"}))
 
 (defn jar [{:keys [build-root jar-file]}]
   (let [class-dir (str build-root "/" "classes")]
@@ -16,7 +17,9 @@
                   :version   version
                   :basis     basis
                   :src-dirs  ["src"]})
-    (b/copy-dir {:src-dirs   ["src" "calendars-generated"]
+    (b/copy-dir {:src-dirs   ["src"]
                  :target-dir class-dir})
+    (b/copy-dir {:src-dirs   ["calendars-generated"]
+                 :target-dir (str class-dir "/" "calendars-generated")})
     (b/jar {:class-dir class-dir
             :jar-file  (str build-root "/" jar-file)})))
