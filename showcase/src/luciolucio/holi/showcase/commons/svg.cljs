9622e68dbbd0a26ac7371471b0e925e6feb4b9d5
Lucio Assis <luciolucio@gmail.com>
2023-02-06 19:12:39 -0300

    Implement basic showcase + cleanup template stuff

--- showcase/src/luciolucio/holi/showcase/commons/svg.cljs
+++
@@ -1,2 +0,0 @@
-(ns luciolucio.holi.showcase.commons.svg
-  (:require-macros [luciolucio.holi.showcase.commons.svg]))
--- showcase/src/luciolucio/holi/showcase/pages/home/core.cljs
+++ showcase/src/luciolucio/holi/showcase/pages/home/core.cljs
@@ -1,8 +1,21 @@
 (ns luciolucio.holi.showcase.pages.home.core
-  (:require [luciolucio.holi.showcase.commons.config :as config]
-            [luciolucio.holi.showcase.commons.style :as style]
+  (:require [luciolucio.holi.showcase.commons.style :as style]
             [luciolucio.holi.showcase.components.layout.stack-container.core :refer [stack-container]]
-            [luciolucio.holi.showcase.components.layout.inline-container.core :refer [inline-container]]))
+            [luciolucio.holi.showcase.components.layout.inline-container.core :refer [inline-container]]
+            [luciolucio.holi :as holi]
+            [goog.string :as gstr]
+            [reagent.core :as r]
+            [tick.core :as t]
+            [tick.locale-en-us]))
+
+(def container-style
+  (style/gen {:label         :route-container-style
+              :display       "flex"
+              :flexDirection "column"
+              :alignItems    "stretch"
+              :position      "relative"
+              :minWidth      "100%"
+              :minHeight     "100vh"}))
 
 (def version-style (style/gen {:label      :version-style
                                :color      "grey"
@@ -21,21 +34,56 @@
 (def header-style (style/gen {:label   :header-style
                               :padding "1em 1em"}))
 
-(defn sidebar-and-main []
-  [inline-container {:spacing "0em"}
-   [stack-container {:spacing "1em"}
-    [:p "Countries"]
-    [:p "Cities"]]
-   [:p "Main"]])
+(def sidebar-and-main-style
+  (style/gen {:label              :sidebar-and-main-container-style
+              :height             "100vh"
+              :padding            "0em 1em"
+              :display            :flex
+              :flexDirection      :row
+              "& > *:first-child" {:flexShrink  0
+                                   :borderRight "1px solid black"
+                                   :width       "25%"
+                                   :marginRight "1em"}
+              "& > *:last-child"  {:flexGrow  1
+                                   :textAlign :center}}))
+
+(defn sidebar [change-calendar]
+  (letfn [(calendar-button [calendar description]
+            [:a {:on-click #(change-calendar calendar description)} (gstr/format "%s (%s)" description calendar)])]
+    [stack-container {:spacing "1em"}
+     [:h2 "Countries"]
+     [calendar-button "BR" "Brazil"]
+     [calendar-button "GB" "Great Britain"]
+     [calendar-button "US" "United States"]
+     [:h2 "Cities"]
+     [calendar-button "brazil/sao-paulo" "São Paulo"]]))
+
+(defn holiday [h]
+  [:p (gstr/format "%s - %s" (t/format "dd/MMM/YYYY" (:date h)) (:name h))])
+
+(defn main-calendar-view [calendar description]
+  [:div
+   [:h1 (gstr/format "%s Holiday Calendar (%s)" calendar description)]
+   [:p "2023"]
+   (into [:<>]
+         (mapv holiday (holi/list-holidays 2023 calendar)))])
+
+(defn sidebar-and-main [calendar-info]
+  (let [[calendar description] @calendar-info
+        change-calendar (fn [calendar description] (reset! calendar-info [calendar description]))]
+    [:div {:class sidebar-and-main-style}
+     [sidebar change-calendar]
+     [main-calendar-view calendar description]]))
 
 (defn header []
   [:div {:class header-style}
    [inline-container {:spacing "0em" :justify "space-between"}
-    [:div "Holi calendar showcase" [version (:version config/config)]]
+    [:div "Holi calendar showcase" [version "0.14.0"]]
     [:a {:href "https://cljdoc.org/d/io.github.luciolucio/holi/CURRENT"} "Docs"]]])
 
-(defn view [_match]
-  [stack-container {:spacing "0em"}
-   [header]
-   [horizontal-line]
-   [sidebar-and-main]])
+(defn view []
+  [:div {:class container-style}
+   [stack-container {:spacing "0em"}
+    [header]
+    [horizontal-line]
+    [sidebar-and-main (r/atom ["US" "United States"])]]])
