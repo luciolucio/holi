8af052e7f0a3d6dd5774116b1a56a8801501b235
Lucio Assis <luciolucio@gmail.com>
2023-02-24 14:06:04 -0300

    Add more performance tests and refactor

--- test/luciolucio/holi/generate_test/generate_performance_test.clj
+++ test/luciolucio/holi/generate_test/generate_performance_test.clj
@@ -8,12 +8,18 @@
 (def root-path "test-resources/file/performance")
 (def output-path "test-output")
 
+; Use for manual runs
+(def run-quick? false)
+
+(defmacro run-benchmark [expression]
+  `(if run-quick?
+     (criterium/quick-benchmark ~expression nil)
+     (criterium/benchmark ~expression nil)))
+
 (ct/deftest ^:performance should-generate-a-datelist-in-under-10-nanoseconds
   (println "Running benchmark for luciolucio.holi.file/generate-datelist!:")
   (let [hol-filename "test-resources/file/performance/BR.hol"
-        benchmark-result (criterium/benchmark
-                          (fn [] (file/generate-datelist! root-path hol-filename output-path 2020 80))
-                          nil)
+        benchmark-result (run-benchmark (file/generate-datelist! root-path hol-filename output-path 2020 80))
         mean-generation-time-in-s (-> benchmark-result :mean first)
         less-than-10-ns? (< mean-generation-time-in-s 10e-9)]
     (criterium/report-result benchmark-result)
@@ -21,14 +27,32 @@
 
     (ct/is less-than-10-ns?)))
 
-(ct/deftest ^:performance should-add-business-days-in-under-50-millis
-  (println "Running benchmark for luciolucio.holi/add:")
-  (let [benchmark-result (criterium/benchmark
-                          (holi/add (LocalDate/of 2023 2 16) 5 :business-days "BR")
-                          nil)
+(ct/deftest ^:performance should-add-business-days-with-calendar-in-under-50-milliseconds
+  (println "Running benchmark for luciolucio.holi/add :business-days with a calendar:")
+  (let [benchmark-result (run-benchmark (holi/add (LocalDate/of 2023 2 16) 5 :business-days "BR"))
         mean-in-s (-> benchmark-result :mean first)
         less-than-50ms? (< mean-in-s 50e-3)]
     (criterium/report-result benchmark-result)
     (println "Did it take less than 50ms?" less-than-50ms?)
 
     (ct/is less-than-50ms?)))
+
+(ct/deftest ^:performance should-add-business-days-with-no-calendar-in-under-50-milliseconds
+  (println "Running benchmark for luciolucio.holi/add :business-days with no calendar:")
+  (let [benchmark-result (run-benchmark (holi/add (LocalDate/of 2023 2 16) 5 :business-days))
+        mean-in-s (-> benchmark-result :mean first)
+        less-than-50ms? (< mean-in-s 50e-3)]
+    (criterium/report-result benchmark-result)
+    (println "Did it take less than 50ms?" less-than-50ms?)
+
+    (ct/is less-than-50ms?)))
+
+(ct/deftest ^:performance should-add-days-in-under-100-nanoseconds
+  (println "Running benchmark for luciolucio.holi/add days without calendars:")
+  (let [benchmark-result (run-benchmark (holi/add (LocalDate/of 2023 2 16) 5 :days))
+        mean-in-s (-> benchmark-result :mean first)
+        less-than-100-ns? (< mean-in-s 100e-9)]
+    (criterium/report-result benchmark-result)
+    (println "Did it take less than 100ns?" less-than-100-ns?)
+
+    (ct/is less-than-100-ns?)))
