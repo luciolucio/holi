3695b138835c1d62b5af29c0f97e86d2dec368e7
Lucio Assis <luciolucio@gmail.com>
2023-02-24 13:46:26 -0300

    Make performance tests a lot more useful with criterium

--- test/luciolucio/holi/generate_test/generate_performance_test.clj
+++ test/luciolucio/holi/generate_test/generate_performance_test.clj
@@ -1,13 +1,34 @@
 (ns luciolucio.holi.generate-test.generate-performance-test
   (:require [clojure.test :as ct]
-            [luciolucio.holi.file :as file]))
+            [criterium.core :as criterium]
+            [luciolucio.holi :as holi]
+            [luciolucio.holi.file :as file])
+  (:import (java.time LocalDate)))
 
 (def root-path "test-resources/file/performance")
 (def output-path "test-output")
 
-(ct/deftest ^:performance should-perform-well
-  (time
-   (dotimes [_ 10]
-     (file/generate-datelist! root-path "test-resources/file/performance/BR.hol" output-path 2020 80)))
+(ct/deftest ^:performance should-generate-a-datelist-in-under-10-nanoseconds
+  (println "Running benchmark for luciolucio.holi.file/generate-datelist!:")
+  (let [hol-filename "test-resources/file/performance/BR.hol"
+        benchmark-result (criterium/benchmark
+                          (fn [] (file/generate-datelist! root-path hol-filename output-path 2020 80))
+                          nil)
+        mean-generation-time-in-s (-> benchmark-result :mean first)
+        less-than-10-ns? (< mean-generation-time-in-s 10e-9)]
+    (criterium/report-result benchmark-result)
+    (println "Did it take less than 10ns?" less-than-10-ns?)
 
-  (ct/is (= 1 1)))
+    (ct/is less-than-10-ns?)))
+
+(ct/deftest ^:performance should-add-business-days-in-under-50-millis
+  (println "Running benchmark for luciolucio.holi/add:")
+  (let [benchmark-result (criterium/benchmark
+                          (holi/add (LocalDate/of 2023 2 16) 5 :business-days "BR")
+                          nil)
+        mean-in-s (-> benchmark-result :mean first)
+        less-than-50ms? (< mean-in-s 50e-3)]
+    (criterium/report-result benchmark-result)
+    (println "Did it take less than 50ms?" less-than-50ms?)
+
+    (ct/is less-than-50ms?)))
